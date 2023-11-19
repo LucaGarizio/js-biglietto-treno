46118a3f6842aeade8965016b3cad645d277f6b1
Luca Garizio <garizioluca2@gmail.com>
2023-11-19 14:17:04 +0100

    risultato frase sconto aggiornata

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
@@ -16,64 +15,53 @@ let risultato = 0;
 
 // NOME UTENTE
 const userName = (prompt("Inserisci il tuo nome"))
-console.log("Nome Utente:", userName);
+// console.log("Nome Utente:", userName);
 
 
 // ETÀ UTENTE
 const userEta = parseInt(prompt ("Inserisci la tua eta"))
-console.log("Eta utente:", userEta);
+// console.log("Eta utente:", userEta);
 
 
 // KM CHE DEVE PERCORRERE
 const userDistance = parseInt(prompt("Inserisci distanza in km"))
-console.log(userDistance);
+// console.log("Kilometri da percorrere:" ,userDistance);
 
 
 // PREZZO  BIGLIETTO  PER KM
 const prezzoKm = 0.21;
-console.log("prezzo per km:", prezzoKm);
+// console.log("prezzo per km:", prezzoKm);
 
 
 // PREZZO BIGLIETTO IN BASE AI KM PERCORSI
 const totalPrice = userDistance * prezzoKm; 
-console.log("prezzo totale km:", totalPrice);
+// console.log("prezzo totale km:", totalPrice);
 
 
-// PREZZO BIGLIETTO SCONTATO 20%
-// if (userEta < 18) {
-//     let minorenne = ((totalPrice * 20) /100);
-//     risultato = totalPrice - minorenne;
-//     console.log("sconto minorenne:", parseFloat(risultato).toFixed(2));
-// } else if (userEta >= 65) {
-//     let maggiorenne = ((totalPrice * 40) /100);
-//     risultato = totalPrice - maggiorenne;
-//     console.log("sconto maggiorenne:", parseFloat(risultato).toFixed(2));
-// } else {
-//     risultato = totalPrice;
-//     console.log("prezzo totale:", totalPrice);
-// }
-
+// VARIABILE SCONTO
 let sconto = 0;
-let frase ="il tuo biglietto costa:";
 
-// PREZZO BIGLIETTO SCONTATO 20%
+
+// VARIABILE FRASE
+let frase ="il prezzo totale del tuo biglietto é di:";
+
+
+// SCONTO 20%
 if (userEta < 18) {
     sconto = ((totalPrice * 20) /100);  
-    console.log("sconto1:", sconto);
-}    
+    frase = "il tuo biglietto scontato del 20% costa:";
+    // console.log("sconto underage:", sconto);
+}
+// SCONTO 40%  
 if (userEta >= 65) {
     sconto = ((totalPrice * 40) /100); 
-    console.log("sconto over:", sconto);
-    
+    frase = "il tuo biglietto scontato del 40% costa:";
+    // console.log("sconto over:", sconto, "£");  
 }
-if (sconto > 0) {
-    frase = " il tuo biglietto scontato costa:";
-    console.log("frase scontata", frase);
-} 
 
 risultato = totalPrice - sconto;
-    console.log("prezzo totale:", totalPrice);
+    // console.log("prezzo totale:", totalPrice);
 
 
 // RISULTATO PREZZO BIGLIETTO IN NUMERI DECIMALI
-document.getElementById("mioBiglietto").innerHTML = `Ciao ${userName} ${frase} ${parseFloat(risultato).toFixed(2)}`;
+document.getElementById("mioBiglietto").innerHTML = `Ciao ${userName} ${frase} ${(risultato)}`;
